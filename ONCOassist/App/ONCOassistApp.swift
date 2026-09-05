30f85174877cdfd7f8067f466115c2faa2d8d1dd
Althaf Bin Abdulla <althaf@sapaad.com>
2026-09-05 07:18:01 +0530

    Initial commit

---
+++ ONCOassist/App/ONCOassistApp.swift
@@ -0,0 +1,17 @@
+//
+//  ONCOassistApp.swift
+//  ONCOassist
+//
+//  Created by Althaf Bin Abdulla on 05/09/26.
+//
+
+import SwiftUI
+
+@main
+struct ONCOAssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("ONCOAssist")
+        }
+    }
+}
